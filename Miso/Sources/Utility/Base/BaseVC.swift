b319c0413ea9f318e8cae56b6c90145b869df372
junseopark0331 <junseopark0331@icloud.com>
2023-10-28 15:06:27 +0900

    :art: BaseVC
    BaseVC

---
+++ Miso/Sources/Utility/Base/BaseVC.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+class BaseVC: UIViewController {
+    // MARK: - Properties
+    let bound = UIScreen.main.bounds
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+        setupBackgroundIfNotSet()
+        addView()
+        setLayout()
+        bind()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setLayoutSubviews()
+    }
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        print("\(type(of: self)): \(#function)")
+    }
+    // MARK: - Method
+    private func setupBackgroundIfNotSet() {
+        if self.view.backgroundColor == nil {
+            self.view.backgroundColor = .white
+        }
+    }
+    
+    func addView() {}
+    func setup() {}
+    func setLayout() {}
+    func setLayoutSubviews() {}
+    func bind() {}
+}
+
+
---
+++ Miso/Sources/Utility/UIColor/UIColor + HexColor.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor + HexColor.swift
+//  Miso
+//
+//  Created by 박준서 on 10/28/23.
+//
+
+import Foundation
