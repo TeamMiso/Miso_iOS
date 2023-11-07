c03faf38c020aa748450db0d0b383448a0ea2933
junseopark0331 <junseopark0331@icloud.com>
2023-11-07 23:52:29 +0900

    :lipstick: PageControl 추가
    Page control cnrk

--- Miso/Sources/Presentation/MainVC.swift
+++ Miso/Sources/Presentation/MainVC.swift
@@ -2,7 +2,15 @@ import UIKit
 import Then
 import SnapKit
 
+protocol CustomPageVCDelegate: class {
+    func numberOfPage(atIndex: Int)
+    func pageChangeTo(atIndex: Int)
+}
+
 final class MainVC: BaseVC {
+    
+    var currentIndex: Int?
+    var pendingIndex: Int?
     
     var individualPageVCList = [UIViewController]()
     
@@ -11,7 +19,12 @@ final class MainVC: BaseVC {
         navigationOrientation: .horizontal
     )
     
-    private lazy var 
+    private lazy var pageControlButton = UIPageControl().then {
+        $0.currentPageIndicatorTintColor = UIColor(rgb: 0x81A895)
+        $0.pageIndicatorTintColor = UIColor(rgb: 0xD9D9D9)
+        $0.numberOfPages = 4
+        $0.currentPage = 0
+    }
     
     private let contentView = UIView()
     
@@ -42,6 +55,7 @@ final class MainVC: BaseVC {
     override func addView() {
         view.addSubviews(
             pageVC.view,
+            pageControlButton,
             contentView
         )
         contentView.addSubview(
@@ -57,8 +71,16 @@ final class MainVC: BaseVC {
         
         contentView.snp.makeConstraints {
             $0.height.equalTo(400)
-            $0.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(188)
-            $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(43)
+            $0.width.equalTo(289)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(188)
+        }
+        
+        pageControlButton.snp.makeConstraints {
+            $0.height.equalTo(5)
+            $0.width.equalTo(160)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(168)
         }
         
     }
@@ -86,8 +108,6 @@ extension MainVC: UIPageViewControllerDataSource {
         } else {
             return individualPageVCList[currentIndexOfPageVC + 1]
         }
-        
-        
     }
     
 }
@@ -95,5 +115,17 @@ extension MainVC: UIPageViewControllerDataSource {
 
 extension MainVC: UIPageViewControllerDelegate {
     
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        
+        pendingIndex = individualPageVCList.firstIndex(of: pendingViewControllers.first!)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            currentIndex = pendingIndex
+            if let index = currentIndex {
+                pageControlButton.currentPage = index
+            }
+        }
+    }
 }
-
