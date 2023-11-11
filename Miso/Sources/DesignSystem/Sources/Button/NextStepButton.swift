9e512ceae6796dd8006bc6786d2f71972c2f7be6
junseopark0331 <junseopark0331@icloud.com>
2023-11-11 14:42:46 +0900

    :lipstick: NextStepButton
    v

---
+++ Miso/Sources/DesignSystem/Sources/Button/NextStepButton.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+public final class NextStepButton: UIButton{
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    func setupView(){
+        titleLabel?.font = .Miso(size: 18, family: .bold)
+        setTitleColor(UIColor(rgb: 0xFFFFFF), for: .normal)
+        layer.cornerRadius = 10
+        clipsToBounds = true
+    }
+}
