e208008d42c96622aeb2e3b6e646832fd42d91db
junseopark0331 <junseopark0331@icloud.com>
2023-11-12 21:28:40 +0900

    :lipstick: Main 페이지 퍼블리싱
    Main 페이지 퍼블리싱

--- Miso/Sources/Presentation/Main/MainVC.swift
+++ Miso/Sources/Presentation/Main/MainVC.swift
@@ -4,6 +4,13 @@ import SnapKit
 
 final class MainVC: BaseVC {
     
+    var option: [OptionEntity] = [
+        OptionEntity(mainImageName: "RegisterRecycle", subImageName: "Camera", mainTitle: "재활용 등록하기.", subTitle: "Camera.", explain: "사진 촬영으로 재활용하는\n방법을 알아보자."),
+        OptionEntity(mainImageName: "ExchangePoint", subImageName: "Market", mainTitle: "포인트 교환하기.", subTitle: "Shop.", explain: "재활용 등록을 통해 얻은\n포인트로 상품을 구매해보자."),
+        OptionEntity(mainImageName: "Inquire", subImageName: "Write", mainTitle: "문의하기.", subTitle: "Inquiry.", explain: "등록되지 않은 재활용\n방법을 등록해보자."),
+        OptionEntity(mainImageName: "ExchangePoint", subImageName: "Write", mainTitle: "문의 내역 보기.", subTitle: "List.", explain: "지금까지 문의한 내역을\n확인해보자.")
+    ]
+    
     private let misoLabel = UILabel().then {
         $0.text = "\"미소\""
         $0.textColor = UIColor(rgb: 0x1C1C1E)
@@ -20,7 +27,7 @@ final class MainVC: BaseVC {
         $0.setImage(UIImage(systemName: "rectangle.portrait.and.arrow.right"), for: .normal)
         $0.tintColor = UIColor(rgb: 0x000000)
     }
-
+    
     private let menuLabel = UILabel().then {
         $0.text = "메뉴"
         $0.textColor = UIColor(rgb: 0x1C1C1E)
@@ -28,8 +35,27 @@ final class MainVC: BaseVC {
         $0.font = .Miso(size: 25, family: .extraLight)
     }
     
+    lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private let flowLayout = UICollectionViewFlowLayout().then{
+        $0.itemSize = CGSize(width: 289, height: 400)
+        $0.scrollDirection = .horizontal
+        $0.minimumLineSpacing = 40
+    }
     
     
+    override func setup() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        self.collectionView.register(OptionCell.self, forCellWithReuseIdentifier: OptionCell.identifier)
+        self.collectionView.collectionViewLayout = flowLayout
+        flowLayout.minimumLineSpacing = 15
+    }
     
     
     override func addView() {
@@ -37,7 +63,8 @@ final class MainVC: BaseVC {
             misoLabel,
             gotoSignupButton,
             logoutButton,
-            menuLabel
+            menuLabel,
+            collectionView
         )
     }
     
@@ -60,8 +87,40 @@ final class MainVC: BaseVC {
             $0.top.equalTo(misoLabel.snp.bottom).offset(86)
             $0.leading.equalTo(misoLabel.snp.leading)
         }
+        collectionView.snp.makeConstraints {
+            $0.height.equalTo(400)
+            $0.top.equalTo(menuLabel.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+}
+ 
+    
+
+extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return option.count
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OptionCell.identifier, for: indexPath)
+        
+        if let cell = cell as? OptionCell {
+            cell.mainImageView.image = UIImage(named: option[indexPath.item].mainImageName)!
+            cell.subImageView.image = UIImage(named: option[indexPath.item].subImageName)!
+            cell.mainTitleLabel.text = option[indexPath.item].mainTitle
+            cell.subTitleLabel.text = option[indexPath.item].subTitle
+            cell.explainLabel.text = option[indexPath.item].explain
+        }
+        
+        return cell
+    }
     
-    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
 }
+
+
--- Miso/Sources/Presentation/Main/PageCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PageCell.swift
-//  Miso
-//
-//  Created by 박준서 on 11/12/23.
-//
-
-import Foundation
