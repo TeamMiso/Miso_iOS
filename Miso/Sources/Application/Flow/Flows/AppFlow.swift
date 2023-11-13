bd5e6d2880bbfeefa2e5c494363289f252143c1f
junseopark0331 <junseopark0331@icloud.com>
2023-11-13 23:37:10 +0900

    :sparkles: App Flow 구현
    App Flow 구현

--- Miso/Sources/Application/Flow/Flows/AppFlow.swift
+++ Miso/Sources/Application/Flow/Flows/AppFlow.swift
@@ -1,8 +1,40 @@
-//
-//  AppFlow.swift
-//  Miso
-//
-//  Created by 박준서 on 11/13/23.
-//
+import UIKit
+import RxFlow
+import RxCocoa
+import RxSwift
 
-import Foundation
+class AppFlow: Flow {
+    
+    var window: UIWindow
+    
+    var root: Presentable {
+        return self.window
+    }
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? DemoStep else { return .none }
+        
+        switch step {
+        case .mainIsRequired:
+            return navigateToMain()
+        case .loginIsRequired
+        }
+    }
+    
+    private func navigateToMain() -> FlowContributors {
+        let vc = MainVC()
+        
+        
+    }
+    
+    private func navigateToLogin() -> FlowContributors {
+        let vc = LoginVC()
+        
+    }
+    
+    
+}
