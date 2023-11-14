2a97bb425392c29fbbd16291a1b6f11af583c105
junseopark0331 <junseopark0331@icloud.com>
2023-11-14 20:44:57 +0900

    :art: MainFlow 추가
    MainFlow 추가

--- Miso/Sources/Application/Flow/Flows/MainFlow.swift
+++ Miso/Sources/Application/Flow/Flows/MainFlow.swift
@@ -1,8 +1,33 @@
-//
-//  MainFlow.swift
-//  Miso
-//
-//  Created by 박준서 on 11/14/23.
-//
+import UIKit
+import RxFlow
 
-import Foundation
+class MainFlow: Flow {
+    
+    var root: Presentable {
+        return self.rootViewController
+    }
+    
+    private lazy var rootViewController: UINavigationController = {
+        let viewController = UINavigationController()
+        return viewController
+    }()
+    
+    init() {}
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? DemoStep else { return .none }
+        switch step {
+        case .mainIsRequired:
+            return self.navigateToMain()
+        case .loginIsRequired:
+            return .end(forwardToParentFlowWithStep: DemoStep.loginIsRequired)
+        }
+    }
+    
+    private func navigateToMain() -> FlowContributors {
+        let vc = MainVC()
+        self.rootViewController.setViewControllers([vc], animated: false)
+        return .one(flowContributor: .contribute(withNext: vc))
+    }
+    
+}
