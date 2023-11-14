17fe8f4337609ed4256debcba0f9263a7c17b9dc
junseopark0331 <junseopark0331@icloud.com>
2023-11-14 20:45:58 +0900

    :art: MainStepper 추가
    MainStepper 추가

--- Miso/Sources/Application/Flow/Flows/MainStepper.swift
+++ Miso/Sources/Application/Flow/Flows/MainStepper.swift
@@ -1,8 +1,18 @@
-//
-//  MainStepper.swift
-//  Miso
-//
-//  Created by 박준서 on 11/14/23.
-//
-
 import Foundation
+import RxFlow
+import RxCocoa
+import RxSwift
+
+class MainStepper: Stepper {
+    
+    let steps = PublishRelay<Step>()
+    
+    private let disposeBag = DisposeBag()
+    
+    init() {
+    }
+    
+    var initialStep: Step {
+        return DemoStep.mainIsRequired
+    }
+}
