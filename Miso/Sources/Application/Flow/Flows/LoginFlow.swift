e6a15d31f99cb8dced44e5f5840de6f094cc8e66
junseopark0331 <junseopark0331@icloud.com>
2023-11-14 20:45:32 +0900

    :art: LoginFlow 추가
    LoginFlow 추가

--- Miso/Sources/Application/Flow/Flows/LoginFlow.swift
+++ Miso/Sources/Application/Flow/Flows/LoginFlow.swift
@@ -1,8 +1,33 @@
-//
-//  LoginFlow.swift
-//  Miso
-//
-//  Created by 박준서 on 11/14/23.
-//
+import UIKit
+import RxFlow
 
-import Foundation
+class LoginFlow: Flow {
+    
+    var root: Presentable {
+        return self.rootViewController
+    }
+    
+    private lazy var rootViewController: UINavigationController = {
+        let vc = UINavigationController()
+        return vc
+    }()
+    
+    init() {}
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? DemoStep else { return .none }
+        switch step {
+        case .loginIsRequired:
+            return self.navigateToLogin()
+        case .mainIsRequired:
+            return .end(forwardToParentFlowWithStep: DemoStep.mainIsRequired)
+        }
+    }
+    
+    private func navigateToLogin() -> FlowContributors {
+        let vc = LoginVC()
+        self.rootViewController.setViewControllers([vc], animated: true)
+        return .one(flowContributor: .contribute(withNext: vc))
+    }
+    
+}
