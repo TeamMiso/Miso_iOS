30eeb91be5eedbc54f114c47b36665e57b0740ca
junseopark0331 <junseopark0331@icloud.com>
2023-11-17 23:16:04 +0900

    :lipstick: SearchVC 생성
    SearchVC 생성

---
+++ Miso/Sources/Presentation/Search/SearchVC.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+final class SearchVC: BaseVC {
+    
+    override func setup() {
+        self.navigationController?.navigationBar.topItem?.title = ""
+        self.navigationController?.navigationBar.tintColor = UIColor(rgb: 0x1C1C1E)
+    }
+    
+//    private let searchTextField = UITextField().then {
+//
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    
+    
+}
