876ce435e814466fbe58ce6dcccf987ee5ec988f
junseopark0331 <junseopark0331@icloud.com>
2023-11-18 22:40:52 +0900

    :art: BaseVM
    BaseVM

---
+++ Miso/Sources/Utility/Base/BaseVM.swift
@@ -0,0 +1,14 @@
+import UIKit
+import RxSwift
+import RxCocoa
+import RxFlow
+import Moya
+
+class BaseViewModel{
+    var disposeBag = DisposeBag()
+    var steps = PublishRelay<Step>()
+//    let keychain = Keychain()
+//    let gomsRefreshToken = GOMSRefreshToken.shared
+//    lazy var accessToken = "Bearer " + (keychain.read(key: Const.KeychainKey.accessToken) ?? "")
+}
+
