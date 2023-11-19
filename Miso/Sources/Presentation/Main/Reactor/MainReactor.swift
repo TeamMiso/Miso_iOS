caec1eeb290e3c9ec4d3e5f79545b95c56d09810
junseopark0331 <junseopark0331@icloud.com>
2023-11-19 22:52:44 +0900

    :sparkles: MainReactor 생성
    MainReactor 생성

--- Miso/Sources/Presentation/Main/Reactor/MainReactor.swift
+++ Miso/Sources/Presentation/Main/Reactor/MainReactor.swift
@@ -1,8 +1,72 @@
-//
-//  MainReactor.swift
-//  Miso
-//
-//  Created by 박준서 on 11/18/23.
-//
-
 import Foundation
+import RxFlow
+import RxCocoa
+import RxSwift
+import ReactorKit
+
+class MainReactor: Reactor {
+    
+    enum Action {
+        case searchButtonDidTap
+        case logoutButtonDidTap
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        var count: Int = 0
+    }
+    
+    var initialState: State
+    
+    init() {
+        self.initialState = State(
+            count: 0
+        )
+    }
+    
+}
+
+// MARK: - Mutate
+extension MainReactor {
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .searchButtonDidTap:
+            return seachButtonDidTap()
+        case .logoutButtonDidTap:
+            return logoutButtonDidTap()
+        }
+    }
+}
+
+// MARK: - Reduce
+extension MainReactor {
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+            
+        }
+    }
+    
+    
+}
+
+
+private extension MainReactor {
+    
+    func seachButtonDidTap() -> Observable<Mutation> {
+        //        self.steps.accept(<#T##Element#>)
+        
+        return .empty()
+    }
+    
+    func logoutButtonDidTap() -> Observable<Mutation> {
+        //        self.steps.accept(<#T##Element#>)
+        return .empty()
+    }
+}
